94ea421f01d3384f3e9eb233626a1a862b32fb9a
jbirrer <birrer.joel@hotmail.ch>
2020-12-15 18:59:46 +0100

    hi

---
+++ EasySorts.java
@@ -0,0 +1,8 @@
+// Bubble Sort
+// This Sorting Algortihm has a squre time complexity
+// it works by going through the array once, for every element, and
+// swapping whenever the next element is smaller than the current
+
+public class EasySorts {
+    
+}

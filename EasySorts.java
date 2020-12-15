e416175695bbf2b1e4cf8d8d7fbe12280222dcba
jbirrer <birrer.joel@hotmail.ch>
2020-12-15 19:23:16 +0100

    bubblesort

--- EasySorts.java
+++ EasySorts.java
@@ -1,8 +1,37 @@
-// Bubble Sort
-// This Sorting Algortihm has a squre time complexity
-// it works by going through the array once, for every element, and
-// swapping whenever the next element is smaller than the current
+import java.util.Arrays;
 
 public class EasySorts {
+    public static void BubbleSort (int[] arr) {
+        int length = arr.length;
+        
+        for(int i = 0; i< length; i++){
+            for(int j = 0; j < length - i - 1; j++){
+                if(arr[j] > arr[j+1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+    }
+
     
+
+    public static void main(String[] args) {
+        int[] test1 = new int[5];
+        for(int i = 0; i < test1.length; i++){
+            test1[i] = test1.length -i;
+        }
+        int[] test2 = test1;
+        BubbleSort(test1);
+        System.out.println(testSorted(test1));
+    }
+    
+    
+    public static boolean testSorted(int[] arr){
+        for(int i = 0; i < arr.length-1; i++){
+            if(arr[i] > arr[i+1]) return false;
+        }
+        return true;
+    }
 }
